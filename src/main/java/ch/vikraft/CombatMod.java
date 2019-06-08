f2b184f822eae4f4b32690eaea0c55464ca91921
Unknown <joan.kuenzler@gmail.com>
2019-06-08 16:34:00 +0200

    mixin fixed

--- src/main/java/ch/vikraft/CombatMod.java
+++ src/main/java/ch/vikraft/CombatMod.java
@@ -8,10 +8,10 @@ import net.minecraft.util.registry.Registry;
 
 public class CombatMod implements ModInitializer {
 
-    public static final Item IRON_WARAXE = new Item(new Item.Settings().group(ItemGroup.COMBAT).maxDamage(6));
+	public static final Item IRON_WARAXE = new Item(new Item.Settings().group(ItemGroup.COMBAT).maxDamage(6));
 
-    @Override
-    public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("vikraft", "iron_waraxe"), IRON_WARAXE);
-    }
+	@Override
+	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier("vikraft", "iron_waraxe"), IRON_WARAXE);
+	}
 }
